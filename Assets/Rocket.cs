8b35c1bba4181373c99f1234acb143e52d65b6ae
Syrus Bomberger <syrusbomberger@yahoo.com>
2021-03-31 16:34:24 -0500

    Rocket

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    private Rigidbody rb;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Launch();
+    }
     public void Launch()
     {
+        StartCoroutine(LaunchSequence());
+    }
 
+    public IEnumerator LaunchSequence()
+    {
+        var timer = 10;
+        yield return new WaitForSeconds(5);
+        while (timer > 0)
+        {
+            Debug.Log(timer);
+            rb.AddForce(Vector3.up * 100000);
+            timer--;
+            yield return new WaitForSeconds(1);
+        }
     }
 }
