63dee9b4592cda539701d6cc28155c936684356f
Syrus Bomberger <syrusbomberger@yahoo.com>
2021-04-19 16:45:19 -0500

    fixed script

--- Assets/Scripts/Rock Scanner/Scanner.cs
+++ Assets/Scripts/Rock Scanner/Scanner.cs
@@ -16,8 +16,7 @@ public class Scanner : MonoBehaviour
         MarsRock script = rock.GetComponent<MarsRock>();
         MonitorScript.DisplayText($"Rock contains {script.Type}\n" +
             $"Weighs {script.Weight}kg");
-        //
-        //ObjectivesManager.Instance.CompleteTask("ScanRocks", 1);
+        ObjectivesManager.Instance.CompleteTask("ScanRocks", 1);
 
     }
 
